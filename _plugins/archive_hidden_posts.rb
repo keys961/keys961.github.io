14ba64d147f2d725e05c51caf1944233c64ee05e
keys961 <keys961@hotmail.com>
2026-05-20 19:39:36 +0800

    xx

--- _plugins/archive_hidden_posts.rb
+++ _plugins/archive_hidden_posts.rb
@@ -1,22 +1,2 @@
-require 'date'
-
-module Jekyll
-  class ArchiveHiddenPostsGenerator < Generator
-    priority :highest
-    safe true
-
-    def generate(site)
-      cutoff_value = site.config['archive_cutoff_date']
-      return if cutoff_value.nil? || cutoff_value.to_s.strip.empty?
-
-      cutoff_date = Date.parse(cutoff_value.to_s)
-      site.posts.docs.each do |post|
-        next unless post.data['date']
-        if post.date.to_date < cutoff_date
-          post.data['hidden'] = true
-          post.data['archived'] = true
-        end
-      end
-    end
-  end
-end
+# This plugin is deprecated. Archive filtering now uses date-based comparison in templates.
+# Kept as empty file for reference.
